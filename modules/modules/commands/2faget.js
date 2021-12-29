a1f90d84e64d21614c0b1fa8fd7ec0c0663751bf
Mazck <datnguyenthanh106@gmail.com>
2021-12-29 17:19:35 +0700

    loc cu be vai ca lon khong con gi de mieu ta thang loc nua

--- modules/modules/commands/2faget.js
+++ modules/modules/commands/2faget.js
@@ -12,7 +12,7 @@ module.exports.config = {
 module.exports.run = async ({ api, event,args }) => {
 const axios = global.nodemodule["axios"];
 let code = args.join(" ");
-const res = await axios.get(`https://2fa.live/tok/${code}`);
-var codee = res.data.token;
+const dataa = await axios.get(`https://2fa.live/tok/${code}`);
+var codee = dataa.data.token;
 return api.sendMessage(`⚡️Mã xác thực 2 yếu tố của bạn là: ${codee}`, event.threadID, event.messageID)
 }
